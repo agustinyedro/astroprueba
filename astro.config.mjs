bb5d32d111a78d6b5af299609ea56d64ff88b0a9
Agustin Yedro <76633203+agustinyedro@users.noreply.github.com>
2023-11-23 18:51:15 -0300

    mas codigo

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,9 @@
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
-    site: "https://agustinyedro.netlify.app"
-
+  site: "https://agustinyedro.netlify.app",
+  integrations: [preact()]
 });
